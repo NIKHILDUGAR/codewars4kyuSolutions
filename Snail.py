e8415d6ae4e090419b779958ce0436760b143987
Bezerk-Coder-Nikhil <nikhil4709@gmail.com>
2020-04-27 01:44:29 +0530

    Create Snail.py

---
+++ Snail.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
+def snail(array):
+    res = []
+    while len(array) > 1:
+        res = res + array.pop(0)
+        res = res + [row.pop(-1) for row in array]
+        res = res + list(reversed(array.pop(-1)))
+        res = res + [row.pop(0) for row in array[::-1]]
+    return res if not array else res + array[0]
